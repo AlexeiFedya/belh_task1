2620f320a5f5712a025d51f75da3976a5ffd5106
AlexeiFedya <fedya5711@icloud.com>
2020-11-10 22:42:46 +0300

    Updates

---
+++ precache-manifest.79eb1e11000b55a497c0fae3c67f9051.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b660495cc85caa85e01f83c9787b48c",
+    "url": "/belh_task1/index.html"
+  },
+  {
+    "revision": "48ca4a9689d2f551e8e4",
+    "url": "/belh_task1/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a47f09cb2f2f591a22fa",
+    "url": "/belh_task1/static/css/main.8dd9f808.chunk.css"
+  },
+  {
+    "revision": "48ca4a9689d2f551e8e4",
+    "url": "/belh_task1/static/js/2.302160dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/belh_task1/static/js/2.302160dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47f09cb2f2f591a22fa",
+    "url": "/belh_task1/static/js/main.0648a216.chunk.js"
+  },
+  {
+    "revision": "1bf0ccc724707431809d",
+    "url": "/belh_task1/static/js/runtime-main.406b5e45.js"
+  },
+  {
+    "revision": "83204dada430192e680cbf2eee4d83c6",
+    "url": "/belh_task1/static/media/call-center.83204dad.svg"
+  },
+  {
+    "revision": "0b83231ab475f8770d6f555b4b7de35d",
+    "url": "/belh_task1/static/media/earth_k.0b83231a.png"
+  },
+  {
+    "revision": "c99272d232d399ceaf61b77da1c51b8e",
+    "url": "/belh_task1/static/media/foto1.c99272d2.png"
+  },
+  {
+    "revision": "e52d1f51282a22e802733582b93936fa",
+    "url": "/belh_task1/static/media/foto2.e52d1f51.png"
+  },
+  {
+    "revision": "a72610f48441d9e8a67cd2c9dc70f020",
+    "url": "/belh_task1/static/media/foto3.a72610f4.png"
+  },
+  {
+    "revision": "bc7d92b984dff796c6029ec91d970302",
+    "url": "/belh_task1/static/media/foto4.bc7d92b9.png"
+  },
+  {
+    "revision": "b78bca04f1ffb7837c3de10894325471",
+    "url": "/belh_task1/static/media/logo1.b78bca04.svg"
+  },
+  {
+    "revision": "bfbd61a9248c7561bb1aa3169cff71cd",
+    "url": "/belh_task1/static/media/mastercard.bfbd61a9.svg"
+  },
+  {
+    "revision": "b8dd3ab10e6f1740c73f62854178f206",
+    "url": "/belh_task1/static/media/paypal.b8dd3ab1.svg"
+  },
+  {
+    "revision": "f7ce5ffce3d15a7001788c2fe3f10ff7",
+    "url": "/belh_task1/static/media/visa.f7ce5ffc.svg"
+  }
+]);
